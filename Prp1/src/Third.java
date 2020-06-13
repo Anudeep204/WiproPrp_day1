f09dee0de31ade158aa057fc60a7d947870fdab1
anudeep <anudeep@anu>
2020-06-13 11:24:45 +0530

    first commmmit

---
+++ Prp1/src/Third.java
@@ -0,0 +1,13 @@
+public class Third
+{
+public static void main(String[] args) 
+{
+if(args.length==0)
+	System.out.println("No values");
+else
+{
+for(String x:args)
+ System.out.print(x+",");
+}
+}
+}
